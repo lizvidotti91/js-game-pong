aabdecf6e2583cd5d6b22d9e274e223cc638de34
lizvidotti91 <liz.vidotti@gmail.com>
2020-08-20 17:00:06 -0300

    Inicializar e resetar a bolinha

--- script/pong.js
+++ script/pong.js
@@ -1,9 +1,17 @@
+// Variáveis de Renderização do Canvas
 var canvasContext;
 var player1Y;
 var player2Y;
 var p1Points;
 var p2Points;
 
+// Variáveis para inicializar e resetar a bolinha
+var ballYOrientation;
+var ballXOrientation;
+var ballX;
+var ballY;
+
+// Variáveis de Renderização do Canvas
 const heightCanvas = 500;
 const widthCanvas = 800;
 const playerWidth = 20;
@@ -19,7 +27,8 @@ function setup(){
     player1Y = player2Y = (heightCanvas/2) - (playerHeight/2);
 
     // Inicializa os pontos dos jogadores como 0
-    p1Points = p2Points = 0;
+    p1Points = 0;
+    p2Points = 0;
 
     // Define um intervalo de 60 fps <<frames por segundo>> para o loop
     setInterval(loop,1000/60);
@@ -27,4 +36,14 @@ function setup(){
 
 function loop(){
 
+}
+
+setup();
+
+function initBall(){
+    console.log(`${p1Points} VS ${p2Points}`);
+    ballYOrientation = Math.pow(2, Math.floor(Math.random()*2)+1)-3;
+    ballXOrientation = Math.pow(2, Math.floor(Math.random()*2)+1)-3;
+    ballX = widthCanvas / 2 - 10;
+    ballY = heightCanvas / 2 - 10; 
 }
